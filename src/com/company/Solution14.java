815e96e5d7c41dfc5dc09818488b0bcaf17597c3
Sushant Mishra <mishrasushant1901@gmail.com>
2022-03-29 23:57:01 +0530

    Best Time to Buy and Sell Stock 4

---
+++ src/com/company/Solution14.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.*;
+class Solution14 {
+    public int maxProfit(int k, int[] prices) {
+        return bestBuy(prices,0, 1, k, new HashMap<String,Integer>());
+    }
+    public int bestBuy(int[] prices, int currentDay, int canBuy, int transCount, HashMap<String,Integer> memo){
+
+        if(currentDay >= prices.length || transCount<=0)
+            return 0;
+
+        String currentKey = Integer.toString(currentDay) + "_" + Integer.toString(canBuy) + "_" + Integer.toString(transCount);
+
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+
+        if(canBuy==1){
+            int idle = bestBuy(prices, currentDay + 1, canBuy, transCount, memo);
+            int buy = bestBuy(prices, currentDay + 1, 0, transCount, memo) - prices[currentDay];
+
+            memo.put(currentKey,Math.max(idle,buy));
+            return memo.get(currentKey);
+        }
+        else
+        {
+            int idle = bestBuy(prices, currentDay + 1, canBuy, transCount, memo);
+            int sell = bestBuy(prices, currentDay + 1, 1, transCount - 1, memo) + prices[currentDay];
+
+            memo.put(currentKey,Math.max(idle,sell));
+            return memo.get(currentKey);
+        }
+
+    }
+}
