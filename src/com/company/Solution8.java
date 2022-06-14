48cec506849cefee08dab8ee2cf5b58a601542bb
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-14 22:58:05 +0530

    Jump game 2 using DP

--- src/com/company/Solution8.java
+++ src/com/company/Solution8.java
@@ -1,27 +1,24 @@
-package com.company;
-import java.util.*;
-class Solution8 {
+class Solution {
     public int jump(int[] nums) {
-        return minJumps(nums,0, new HashMap<Integer,Integer>());
-
+        return minJumps(nums, 0, new HashMap<Integer,Integer>());
     }
-    private int minJumps(int[] nums,int currentIndex,HashMap<Integer,Integer> memo){
-        if(currentIndex >= nums.length -1){
+    public int minJumps(int[] nums, int currentIndex, HashMap<Integer, Integer> memo){
+        if(currentIndex >= nums.length - 1){
             return 0;
-
         }
-        if(nums[currentIndex]== 0){
-            return 100001;
-        }
-        int currentKey=currentIndex;
+        
+        int currentKey = currentIndex;
+        
         if(memo.containsKey(currentKey))
             return memo.get(currentKey);
-
-        int currentJumps =nums[currentIndex];
-        int ans=100001;
-        for(int x=1;x<=currentIndex;x++){
-            int tempAns =1+minJumps(nums,currentIndex+x,memo);
-            ans =Math.min(ans,tempAns);
+        
+        int currentJump = nums[currentIndex];
+        
+        int ans = 10001;
+        
+        for(int i=1; i <= currentJump; i++){
+            int tempAns = 1 + minJumps(nums, currentIndex + i, memo);
+            ans = Math.min(tempAns, ans);
         }
         memo.put(currentKey,ans);
         return ans;
