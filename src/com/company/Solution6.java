7aae287bef743aca694d5c9a480b1e99bf1e6010
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-14 11:54:39 +0530

    Jump game with greedy approach

--- src/com/company/Solution6.java
+++ src/com/company/Solution6.java
@@ -1,28 +1,12 @@
-/*package com.company;
-class Solution6 {
-    public int change(int amount, int[] coins) {
-        return totalWays(coins,0,amount,new HashMap<String,Integer>());
-
-    }
-    private int totalWays(int[] coins,int currentIndex,int amount, HashMap<String,Integer>memo){
-        if(amount ==0)
-            return 1;
-        if(currentIndex == coins.length)
-            return 0;
-        String currentKey= Integer.toString(currentIndex)+"-"+Integer.toString(amount);
-        if(memo.containsKey(currentKey))
-            return memo.get(currentKey);
-
-        int currentCoin = coins[currentIndex];
-        int consider=0;
-
-        if(currentCoin<= amount)
-            consider= totalWays(coins,currentIndex,amount-currentCoin,memo);
-
-        int notConsider= totalWays(coins,currentIndex+1,amount,memo);
-
-        memo.put(currentKey,consider+notConsider);
-        return consider + notConsider;
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal=nums.length-1;
+        for(int i=nums.length-1;i>=0;i--){
+            if(i+nums[i]>=goal)
+                goal=i;
+        }
+        if(goal==0) return true;
+        else return false;
+        
     }
 }
-*/
