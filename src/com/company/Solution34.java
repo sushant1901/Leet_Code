9b1d0fe9f1aa659c642359dbb378e4fa2d7feb28
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-14 23:31:13 +0530

    jump game 2 using greedy algoritm

---
+++ src/com/company/Solution34.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int res=0;
+        int l=0;
+        int r=0;
+        while(r< nums.length -1){
+            int far=0;
+            for(int i=l;i<r+1;i++){
+                far=Math.max(far,i+nums[i]);
+            }
+            l=r+1;
+            r=far;
+            res+=1;
+            
+        }
+        return res;
+        
+    }
+}
