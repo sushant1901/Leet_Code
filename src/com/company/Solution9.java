359df8e6d8c7126560174dd7297e730844b31511
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-16 20:33:57 +0530

    Unique Path 2

--- src/com/company/Solution9.java
+++ src/com/company/Solution9.java
@@ -1,23 +1,24 @@
-package com.company;
-import java.util.HashMap;
-class Solution9 {
+class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return totalWays(0,0,obstacleGrid.length,obstacleGrid[0].length,obstacleGrid,new HashMap<String ,Integer>() );
+        return totalWays(0,0,obstacleGrid.length,obstacleGrid[0].length,obstacleGrid,new HashMap<String,Integer>());
+        
     }
-
-    private int totalWays(int row,int col,int m,int n,int[][]obstacleGrid,HashMap<String,Integer> memo){
-        if(row== m-1 && col==n-1 && obstacleGrid[row][col] !=1 )
-            return 1;
-        if(row>= m || col >=n || obstacleGrid[row][col]==1)
+    private int totalWays(int currentRow,int currentCol,int m,int n,int[][]obstacleGrid ,HashMap<String,Integer>memo){
+        if(currentRow >=m || currentCol >=n || obstacleGrid[currentRow][currentCol]==1)
             return 0;
-        String  currentKey = row+"-"+col;
+        if(currentRow ==m-1 && currentCol==n-1)
+            return 1;
+        String currentKey=currentRow+"-"+currentCol;
         if(memo.containsKey(currentKey))
             return memo.get(currentKey);
-        int rightMove =totalWays(row,col+1,m,n,obstacleGrid,memo);
-        int downMove =totalWays(row+1,col,m,n,obstacleGrid,memo);
-
+        
+        
+        
+        int rightMove=totalWays(currentRow,currentCol+1,m,n,obstacleGrid,memo);
+        int downMove=totalWays(currentRow+1,currentCol,m,n,obstacleGrid,memo);
         memo.put(currentKey,rightMove+downMove);
+        
         return rightMove+downMove;
-
+        
     }
 }
