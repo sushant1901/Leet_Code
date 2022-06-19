a82e5832da9f04fad1a490923545409a50aa5190
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-19 11:28:02 +0530

    Maximum Square

--- src/com/company/Solution37.java
+++ src/com/company/Solution37.java
@@ -1,34 +1,35 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int area = 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] memo = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='1'){
-                    int side = maxLength(matrix, i, j, m, n, memo);
-                    area = Math.max(area,side*side);
+        
+        int m=matrix.length;
+        int n=matrix[0].length;
+        HashMap<String,Integer>memo=new HashMap<String,Integer>();
+        
+        int area=0;
+        for(int currentRow=0 ; currentRow < m ; currentRow++){
+            for(int currentCol= 0 ; currentCol < n ; currentCol++){
+                if(matrix[currentRow][currentCol] =='1'){
+                     int sideLength=maximumSize(matrix, currentRow, currentCol, m, n,memo);
+                     area=Math.max(area,sideLength*sideLength);
                 }
             }
         }
+        
         return area;
     }
-    
-    public int maxLength(char[][] matrix,int currentRow, int currentCol, int m, int n, int[][] memo){
-        if(currentRow < 0 || currentRow >=m || currentCol < 0 || currentCol >= n || matrix[currentRow][currentCol]=='0'){
-            return 0;
-        }
+    private int maximumSize(char[][]matrix,int currentRow,int currentCol,int m,int n,HashMap<String,Integer>memo){
         
-        if(memo[currentRow][currentCol]!=0)
-            return memo[currentRow][currentCol];
-        int rightMove = 1 + maxLength(matrix, currentRow, currentCol + 1, m, n, memo);
+        if(currentRow<0||currentRow>=m||currentCol<0||currentCol>=n|| matrix[currentRow][currentCol]=='0') return 0;
+        String currentKey=currentRow+"-"+currentCol;
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
         
-        int downMove = 1 + maxLength(matrix, currentRow + 1, currentCol, m, n, memo);
+        int right=1+ maximumSize(matrix,currentRow,currentCol+1,m,n,memo);
+        int down=1+ maximumSize(matrix,currentRow+1,currentCol,m,n,memo);
+        int rightDiag=1+ maximumSize(matrix,currentRow+1,currentCol+1,m,n,memo);
         
-        int drMove = 1 + maxLength(matrix, currentRow + 1, currentCol + 1, m, n, memo);
+        memo.put(currentKey,Math.min(right,Math.min(down,rightDiag)));
         
-        memo[currentRow][currentCol] = Math.min(rightMove, Math.min(downMove,drMove));
-        return memo[currentRow][currentCol];
+        return Math.min(right,Math.min(down,rightDiag));
     }
 }
