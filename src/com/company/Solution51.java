a4a9afcabed24e9b09ca0ab077b913409a435606
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-07-24 23:49:03 +0530

    Merge Two Binary Trees

---
+++ src/com/company/Solution51.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return merge(root1,root2);
+        
+    }
+    private TreeNode merge(TreeNode t1,TreeNode t2){
+        if(t1==null) return t2;
+        if(t2==null) return t1;
+        TreeNode newNode=new TreeNode(t1.val+t2.val);
+        newNode.left=merge(t1.left,t2.left);
+        newNode.right=merge(t1.right,t2.right);
+        
+        return newNode;
+        
+    }
+}
