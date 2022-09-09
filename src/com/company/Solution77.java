279c9996afdb52e67a5c55f980fa15183dc9eb00
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-09-09 23:26:39 +0530

    Power of two (brute force)

--- src/com/company/Solution77.java
+++ src/com/company/Solution77.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-          if(n==0) return false;
-        
+        if(n==0) return false;
         while(n%2 ==0) n/=2;
         
         return n==1;
