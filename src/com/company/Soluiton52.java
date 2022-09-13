8a6e570b66cc31acf3169aed432ba7e41c2852cc
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-09-13 18:10:59 +0530

    Sum of root to leaf in binary tree

--- src/com/company/Soluiton52.java
+++ src/com/company/Soluiton52.java
@@ -1,31 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
-        
-        if(root == null){
-            return 0;
-        }
-        
-        return rootToLeaf(root, "");
+        int[] answer={0};
+        rootToLeaf(root,""+Integer.toString(root.val),answer);
+        return answer[0];
     }
-    
-    private int rootToLeaf(TreeNode root, String currentString){
-        if(root.left == null && root.right == null){
-            
-            return Integer.parseInt(currentString + String.valueOf(root.val), 2);
+    private void rootToLeaf(TreeNode root,String currentPath,int[] answer){
+        if(root.left == null && root.right==null){
+            answer[0]+= Integer.parseInt(currentPath,2);
+            return ;
         }
         
-        int leftSum = 0;
-        int rightSum = 0;
-        
-        if(root.left != null){
-            leftSum =  rootToLeaf(root.left, currentString + String.valueOf(root.val));
+        if(root.left !=null){
+            rootToLeaf(root.left,currentPath+Integer.toString(root.left.val),answer);
+        }
+        if(root.right !=null){
+            rootToLeaf(root.right,currentPath+Integer.toString(root.right.val),answer);
         }
         
-        if(root.right != null){
-            rightSum = rootToLeaf(root.right, currentString + String.valueOf(root.val));
-        }
-        return  leftSum + rightSum;
-        
+        return;
     }
-    
 }
