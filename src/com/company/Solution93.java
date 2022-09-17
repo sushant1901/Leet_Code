e07f68013f94cdefbf191ef183a97036cf5c386a
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-09-17 12:33:06 +0530

    invert Tree

---
+++ src/com/company/Solution93.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        
+        TreeNode LeftAns=invertTree(root.left);
+         TreeNode RightAns=invertTree(root.right);
+        
+        root.left=RightAns;
+        root.right=LeftAns;
+        return root;
+        
+    }
+}
