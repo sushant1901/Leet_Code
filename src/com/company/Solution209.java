61d4812eafa635246530fd7fb75ca7cb1669f99a
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-11-25 22:42:08 +0530

    Sum of Subarray Minimums

---
+++ src/com/company/Solution209.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+	    Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        long res = 0;
+        for(int i = 0; i < arr.length; i++) {
+            while(stack.size() > 1 && arr[stack.peek()] > arr[i]) {
+                int currElemIndex = stack.pop();
+                res += ((long) arr[currElemIndex] * (currElemIndex - stack.peek()) * ( i - currElemIndex)) % 1000000007;
+                res = res % 1000000007;
+            }
+            stack.push(i);
+        }
+        int rightBoundary = arr.length;
+    
+        while(stack.size() > 1) {
+            int currElemIndex = stack.pop();
+            res += ((long) arr[currElemIndex] * (currElemIndex - stack.peek()) * (rightBoundary - currElemIndex)) % 1000000007;
+            res = res % 1000000007;
+        }
+        
+        return (int) res;
+        
+    }
+}
