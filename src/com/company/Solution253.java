9c7c80ea9d35709646c3bba993c9cb1333bf0e53
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-02-07 11:11:44 +0530

    Course Schedule ||

---
+++ src/com/company/Solution253.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> graph= constructGraph(numCourses,prerequisites);
+        int[] indegree= new int[numCourses];
+        
+        for(int currentCourse=0;currentCourse<numCourses;currentCourse++){
+            for(int currentNeigh:graph.get(currentCourse))
+                indegree[currentNeigh]+=1;
+        }
+        
+        Queue<Integer> q= new LinkedList<>();
+        ArrayList<Integer>order= new ArrayList<>();
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.add(i);
+        }
+        
+        boolean[] visited = new boolean[numCourses];
+        
+        while(!q.isEmpty()){
+            int currentCourse=q.remove();
+            
+            if(visited[currentCourse])continue;
+            
+            visited[currentCourse]=true;
+            
+            order.add(currentCourse);
+            for(int currentNeigh:graph.get(currentCourse)){
+                indegree[currentNeigh]-=1;
+                if(indegree[currentNeigh]==0) q.add(currentNeigh);
+            }
+        }
+        
+        
+        if(order.size()!= numCourses) return new int[0];
+        
+        int [] answer= new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            answer[i]=order.get(i);
+        }
+        
+        return answer;
+    }
+    
+    private ArrayList<ArrayList<Integer>> constructGraph(int n,int[][] edges){
+        ArrayList<ArrayList<Integer>> graph= new ArrayList<>();
+        for(int i=0;i<n;i++)
+            graph.add(new ArrayList<Integer>());
+        
+        for(int[] currentEdge: edges){
+            int a=currentEdge[0];
+            int b=currentEdge[1];
+            
+            graph.get(b).add(a);
+        }
+        
+        return graph;
+    }
+}
