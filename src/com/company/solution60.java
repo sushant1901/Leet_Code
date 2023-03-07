fbca4295374801c468fff75b23456cb46cd971b0
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-03-07 11:28:07 +0530

    First bad version

--- src/com/company/solution60.java
+++ src/com/company/solution60.java
@@ -1,12 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int lo=1,hi=n;
-        while(lo<hi){
-            int mid= lo+(hi-lo)/2;
-            if( isBadVersion(mid)) hi=mid;
-            else lo=mid+1;
+        int firstBad=-1;
+        int start=0;
+        int end=n;
+        while(start <= end){
+            int mid= start+(end-start)/2;
             
+            if(isBadVersion(mid)){
+                firstBad=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
         }
-        return lo;
+        return firstBad;
     }
 }
