efcf2bfb696e83049605fee46a4aa096fa8f5ddd
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-03-08 15:36:25 +0530

    Guess Number Higher or Lower II

---
+++ src/com/company/Solution279.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int getMoneyAmount(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int jminusi = 1; jminusi < n; jminusi++) {
+            for (int i = 0; i + jminusi <= n; i++) {
+                int j = i + jminusi;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                                        k + Math.max(k - 1 >= i ? dp[i][k - 1] : 0,
+                                                     j >= k + 1 ? dp[k + 1][j] : 0));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+}
