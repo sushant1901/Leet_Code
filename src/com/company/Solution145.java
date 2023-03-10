a269e99e85e6d0a8b130e0176732de1036f26a7d
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-03-11 01:21:47 +0530

    Search in 2d matrix

--- src/com/company/Solution145.java
+++ src/com/company/Solution145.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-	
-	int row_num = matrix.length;
-	int col_num = matrix[0].length;
-	
-	int begin = 0, end = row_num * col_num - 1;
-	
-	while(begin <= end){
-		int mid = (begin + end) / 2;
-		int mid_value = matrix[mid/col_num][mid%col_num];
-		
-		if( mid_value == target){
-			return true;
-		
-		}else if(mid_value < target){
-			//Should move a bit further, otherwise dead loop.
-			begin = mid+1;
-		}else{
-			end = mid-1;
-		}
-	}
-	
-	return false;
-}
+        int m=matrix.length;
+        int n=matrix[0].length;
+        
+        int start=0;
+        int end=m*n-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int row=mid/n;
+            int col=mid%n;
+            
+            if(matrix[row][col]==target) return true;
+            if(matrix[row][col]>target) end=mid-1;
+            else start=mid+1;
+            
+        }
+        return false;
+    }
 }
