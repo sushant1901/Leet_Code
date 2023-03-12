fef85bad88a05c0d01b198248f1a1479755cb91a
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-03-12 21:36:34 +0530

    Minimum Common Value

---
+++ src/com/company/Solution285.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+
+    int i=0,j=0;
+    while(i<nums1.length && j<nums2.length)
+    {
+        if(nums1[i]<nums2[j]) i++;
+        else if(nums1[i]>nums2[j]) j++;
+        else return nums1[i];
+    }
+    return -1;    
+    }
+}
