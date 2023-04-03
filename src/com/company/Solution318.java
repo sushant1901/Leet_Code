435916ba2e9725a1cd89fe0410fc6b0677eb521e
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-03 20:28:45 +0530

    Longest Substring with At Most K Distinct Characters

---
+++ src/com/company/Solution318.java
@@ -0,0 +1,41 @@
+//Longest Substring with At Most K Distinct Characters--> at lint code.
+import java.util.*;
+public class hashMap2 {
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            String str = scn.next();
+            int k = scn.nextInt();
+            System.out.println(lengthOfLongestSubstringKDistinct(str,k));
+        }
+    }
+    public static  int lengthOfLongestSubstringKDistinct(String str, int k) {
+        
+        HashMap<Character,Integer> memo = new HashMap<>();
+        int answer=0;
+        int distinct =0;
+        int release =0;
+
+        for(int acquire =0;acquire< str.length();acquire++){
+            Character currentChar= str.charAt(acquire);
+            if(memo.containsKey(currentChar)){
+                memo.put(currentChar,memo.get(currentChar)+1);
+            }else{
+                memo.put(currentChar,1);
+                distinct++;
+            }
+
+            while(release < acquire && distinct > k){
+                Character discardChar = str.charAt(release);
+                release+=1;
+
+                memo.put(discardChar,memo.get(discardChar)-1);
+                if(memo.get(discardChar) == 0){
+                    memo.remove(discardChar);
+                    distinct-=1;
+                }
+            }
+            answer=Math.max(answer,acquire-release+1);
+        }
+        return answer;
+    }
+}
