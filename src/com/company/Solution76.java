337545ce0199bed9ca1b9d31b24bc1d460cd6d36
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-10 23:02:16 +0530

    Two Sum

--- src/com/company/Solution76.java
+++ src/com/company/Solution76.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result= new int[2];
-        HashMap<Integer,Integer> memo= new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(memo.containsKey(target-nums[i])){
-                result[1]=i;
-                result[0]=memo.get(target-nums[i]);
+        HashMap<Integer,Integer> memo= new HashMap<>();
+        for( int i=0;i<nums.length;i++){
+            int complement= target-nums[i];
+            if(memo.containsKey(complement)){
+                return new int[]{memo.get(complement),i};
             }
             memo.put(nums[i],i);
         }
-        return result;
+        return new int[2];
+
     }
 }
