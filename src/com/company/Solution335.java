74601a1d3f3daeba7e17b9f64977a7fb70707ce2
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-12 22:20:19 +0530

    Rabbit in the forest

--- src/com/company/Solution335.java
+++ src/com/company/Solution335.java
@@ -29,3 +29,25 @@ class Solution {
         return c;
     }
 }
+
+
+// 2nd approach:
+
+class Solution {
+    public int numRabbits(int[] answers) {
+        double answer=0;
+        HashMap<Integer,Integer> memo= new HashMap<>();
+        for(int currentReportee : answers){
+            if(memo.containsKey(currentReportee)) memo.put(currentReportee,memo.get(currentReportee)+1);
+            else memo.put(currentReportee,1);
+        }
+
+        for(Integer currentKey : memo.keySet()){
+            double groupSize=currentKey+1;
+            double numGroups= Math.ceil(memo.get(currentKey)/groupSize);
+            answer+= (numGroups * groupSize);
+        }
+
+        return (int)answer;
+    }
+}
