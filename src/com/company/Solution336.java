95199d77c16f1f17e2515516ded470f221608c53
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-12 22:23:01 +0530

    Simplify Path

---
+++ src/com/company/Solution336.java
@@ -0,0 +1,17 @@
+class Solution {
+   public String simplifyPath(String path) {
+    Stack<String> stack = new Stack<>();
+    Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
+    for (String dir : path.split("/")) {
+        if (dir.equals("..") && !stack.isEmpty())
+         stack.pop();
+        else if (!skip.contains(dir))
+         stack.push(dir);
+    }
+    StringBuilder res = new StringBuilder();
+    while(!stack.isEmpty()){
+            res.insert(0,stack.pop()).insert(0,"/");
+        }
+    return res.isEmpty() ? "/" : res.toString();
+    }
+}
