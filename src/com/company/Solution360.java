f06bbe4f8d0e75b3f3516b3a71cc6238ec9b0daa
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-29 14:07:00 +0530

    Similar String Groups

---
+++ src/com/company/Solution360.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void dfs(int node, Map<Integer, List<Integer>> adj, boolean[] visit) {
+        visit[node] = true;
+        if (!adj.containsKey(node)) {
+            return;
+        }
+        for (int neighbor : adj.get(node)) {
+            if (!visit[neighbor]) {
+                visit[neighbor] = true;
+                dfs(neighbor, adj, visit);
+            }
+        }
+    }
+
+    public boolean isSimilar(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff == 0 || diff == 2;
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    adj.computeIfAbsent(i, k -> new ArrayList<Integer>()).add(j);
+                    adj.computeIfAbsent(j, k -> new ArrayList<Integer>()).add(i);
+                }
+            }
+        }
+
+        boolean[] visit = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(i, adj, visit);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
