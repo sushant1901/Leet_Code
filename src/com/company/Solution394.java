88d8b5c4e8cb8dc03cb923e5f3d16c2eb2125418
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-02 09:09:19 +0530

    Detonate the Maximum Bombs

---
+++ src/com/company/Solution394.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int n = bombs.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
+                int xj = bombs[j][0], yj = bombs[j][1];
+
+                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
+                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int count = dfs(i, new HashSet<>(), graph);
+            answer = Math.max(answer, count);
+        }
+
+        return answer;
+    }
+    private int dfs(int cur, Set<Integer> visited, Map<Integer, List<Integer>> graph) {
+        visited.add(cur);
+        int count = 1;
+        for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
+            if (!visited.contains(neib)) {
+                count += dfs(neib, visited, graph);
+            }
+        }
+        return count;
+    }
+}
