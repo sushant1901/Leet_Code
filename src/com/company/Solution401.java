8658ebad69bb27d40397e766d498e059caf960e9
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-09 16:16:59 +0530

    Find Smallest Letter Greater Than Target

--- src/com/company/Solution401.java
+++ src/com/company/Solution401.java
@@ -1,3 +1,4 @@
+// Brute Force
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         for (char letter : letters) {
@@ -6,5 +7,23 @@ class Solution {
             }
         }
         return letters[0];
+    }
+}
+
+// binary search
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1, mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left == letters.length ? letters[0] : letters[left];
     }
 }
