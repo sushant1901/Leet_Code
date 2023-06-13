a7c72e399a1ff98835adb1f90526e0f142f92a65
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-13 20:55:46 +0530

    Equal row and column pair

--- src/com/company/Solution407.java
+++ src/com/company/Solution407.java
@@ -1,3 +1,4 @@
+//brute force
 class Solution {
     public int equalPairs(int[][] grid) {
         int count = 0, n = grid.length;
@@ -20,6 +21,28 @@ class Solution {
             }
         }
         
+        return count;
+    }
+}
+
+// HashMap
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int count = 0;
+        int n = grid.length;
+        Map<String, Integer> rowCounter = new HashMap<>();
+        for (int[] row : grid) {
+            String rowString = Arrays.toString(row);
+            rowCounter.put(rowString, 1 + rowCounter.getOrDefault(rowString, 0));
+        }
+        for (int c = 0; c < n; c++) {
+            int[] colArray = new int[n];
+            for (int r = 0; r < n; ++r) {
+                colArray[r] = grid[r][c];
+            }
+            count += rowCounter.getOrDefault(Arrays.toString(colArray), 0);
+        }
+
         return count;
     }
 }
