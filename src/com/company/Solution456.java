00748d3bceb80b79f78420bc4558e5da1fb6fb7c
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-08-10 01:18:41 +0530

    Minimize the Maximum Difference of Pairs

---
+++ src/com/company/Solution456.java
@@ -0,0 +1,29 @@
+class Solution {
+    private int countValidPairs(int[] nums, int threshold) {
+        int index = 0, count = 0;
+        while (index < nums.length - 1) {
+            if (nums[index + 1] - nums[index] <= threshold) {
+                count++;
+                index++;
+            }
+            index++;
+        }
+        return count;
+    }
+
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int left = 0, right = nums[n - 1] - nums[0];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (countValidPairs(nums, mid) >= p) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
